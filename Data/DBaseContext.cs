3930b075d5e70f3e8bd610f68ef0891c67d4d79e
Gabriel Alexandre <104833986+Scoorpioon@users.noreply.github.com>
2023-12-28 15:39:09 -0300

    First commit

---
+++ Data/DBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data;
+
+public class DBaseContext : DbContext
+{
+    public DBaseContext(DbContextOptions<DBaseContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<ContactModel> Contacts { get; set; }
+}
